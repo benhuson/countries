6d238650eb7f1bb0b30d0de65e5233f5e361ff59
Ben Huson <ben@thewhiteroom.net>
2012-06-20 10:20:18 +0100

    Add menu icon.

--- country.php
+++ country.php
@@ -64,41 +64,42 @@ if(is_admin()){
 }
 
 
-/* create custompost type and custom taxonomy */
-function country_init() {	
-
+/**
+ * Create custom post type and custom taxonomy
+ * http://codex.wordpress.org/Function_Reference/register_post_type
+ */
+function country_init() {
 	$labels = array(
-		'name' => _x( 'Countries', 'post type general name' ),
-		'singular_name' => _x( 'Country', 'post type singular name' ),
-		'add_new' => _x( 'Add New', 'Country' ),
-		'add_new_item' => __( 'Add New Country' ),
-		'edit_item' => __( 'Edit Country' ),
-		'new_item' => __( 'New Country' ),
-		'view_item' => __( 'View Country' ),
-		'search_items' => __( 'Search Countries' ),
-		'not_found' =>  __( 'No Countries found' ),
+		'name'               => _x( 'Countries', 'post type general name' ),
+		'singular_name'      => _x( 'Country', 'post type singular name' ),
+		'add_new'            => _x( 'Add New', 'Country' ),
+		'add_new_item'       => __( 'Add New Country' ),
+		'edit_item'          => __( 'Edit Country' ),
+		'new_item'           => __( 'New Country' ),
+		'view_item'          => __( 'View Country' ),
+		'search_items'       => __( 'Search Countries' ),
+		'not_found'          =>  __( 'No Countries found' ),
 		'not_found_in_trash' => __( 'No Countries found in Trash' ), 
-		'parent_item_colon' => ''
+		'parent_item_colon'  => '',
+		'menu_name'          => __( 'Countries' )
 	);
-
 	$args = array(
-		'labels' => $labels,
-		'public' => true,
-		'publicly_queryable' => true,
-		'show_ui' => true,  
-		'query_var' => true,
-		'rewrite' => true,
-		'capability_type' => 'post',
-		'hierarchical' => false,
-		'show_in_nav_menus'=>false,
-		'menu_position' => null,
-		'supports' => array('title'),
-		'visible' =>true,
+		'labels'               => $labels,
+		'public'               => true,
+		'publicly_queryable'   => true,
+		'show_ui'              => true,  
+		'query_var'            => true,
+		'rewrite'              => true,
+		'capability_type'      => 'post',
+		'hierarchical'         => false,
+		'show_in_nav_menus'    => false,
+		'menu_position'        => null,
+		'menu_icon'            => plugins_url( 'images/menu-icon.png', __FILE__ ),
+		'supports'             => array( 'title' ),
+		'visible'              => true,
 		'register_meta_box_cb' => 'country_meta_boxes'
 	);
-	
-	// http://codex.wordpress.org/Function_Reference/register_post_type
-	register_post_type('countries', $args);
+	register_post_type( 'countries', $args );
 }
 
 
